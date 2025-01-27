71004271d67e8a6c48862b5ae4a9b7df84a399ff
Martin Klepsch <martinklepsch@googlemail.com>
2025-01-28 00:07:32 +0200

    better bb task

--- src/feature_flags/task.clj
+++ src/feature_flags/task.clj
@@ -4,14 +4,14 @@
             [feature-flags.core :as ff]))
 
 (defn prune
-  []
+  [& args]
   (let [spec {:spec {:file {:desc "Single file to process"
                             :ref "<path>"}
                      :pattern {:desc "Glob pattern to find files"
                                :ref "<pattern>"}
                      :dry-run {:desc "Only print the transformed result"
                                :coerce :boolean}}}
-        opts (cli/parse-opts *command-line-args* spec)]
+        opts (cli/parse-opts args spec)]
     (if (and (not (:file opts))
              (not (:pattern opts)))
       (println (str "Please provide either --file or --pattern\n\n"

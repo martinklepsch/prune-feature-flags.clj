ac79ce546998b7c72e88763f54776c4f068cc0eb
Martin Klepsch <martinklepsch@googlemail.com>
2025-01-27 23:46:42 +0200

    better bb task

---
+++ src/feature_flags/task.clj
@@ -0,0 +1,31 @@
+(ns feature-flags.task
+  (:require [babashka.cli :as cli]
+            [babashka.fs :as fs]
+            [feature-flags.core :as ff]))
+
+(defn prune
+  []
+  (let [spec {:spec {:file {:desc "Single file to process"
+                            :ref "<path>"}
+                     :pattern {:desc "Glob pattern to find files"
+                               :ref "<pattern>"}
+                     :dry-run {:desc "Only print the transformed result"
+                               :coerce :boolean}}}
+        opts (cli/parse-opts *command-line-args* spec)]
+    (if (and (not (:file opts))
+             (not (:pattern opts)))
+      (println (str "Please provide either --file or --pattern\n\n"
+                    (cli/format-opts spec)))
+      (let [files (if-let [pattern (:pattern opts)]
+                    (fs/glob "." pattern)
+                    [(:file opts)])
+            mapping {'(use-new-name?) true
+                     '(use-new-layout?) true
+                     '(use-beta-features?) true}]
+        (doseq [file files]
+          (println "Processing" (str file))
+          (let [content (slurp file)
+                updated (ff/prune-conditionals content mapping)]
+            (if (:dry-run opts)
+              (println updated)
+              (spit file updated))))))))
